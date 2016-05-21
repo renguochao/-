a95de97a0481b89f881739815e9afb01a606dee3
Guochao Ren <314017427@qq.com>
2016-05-21 16:33:44 +0800

    评论音频显示

--- BSBDQJ/Classes/Essence/Model/RGCComment.h
+++ BSBDQJ/Classes/Essence/Model/RGCComment.h
@@ -12,6 +12,8 @@
 @interface RGCComment : NSObject
 /** 音频文件时长 */
 @property (nonatomic, assign) NSInteger voicetime;
+/** 音频文件路径 */
+@property (nonatomic, copy) NSString *voiceuri;
 /** 评论的文字内容 */
 @property (nonatomic, copy) NSString *content;
 /** 被点赞的数量 */
